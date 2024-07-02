450f8e1b0467f177bb5ae4bd05f05473ff8951a6
josewdf <60181010+josemlwdf@users.noreply.github.com>
2024-07-02 13:05:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,43 +10,43 @@ sudo apt update
 # Check and install seclists
 if ! command_exists seclists; then
     echo "Installing seclists..."
-    sudo apt install seclists -y
+    sudo apt install seclists -y;
 fi
 
 # Check and install nmap
 if ! command_exists nmap; then
     echo "Installing nmap..."
-    sudo apt install nmap -y
+    sudo apt install nmap -y;
 fi
 
 # Check and install gobuster
 if ! command_exists gobuster; then
     echo "Installing gobuster..."
-    sudo apt install gobuster -y
+    sudo apt install gobuster -y;
 fi
 
 # Check and install feroxbuster
 if ! command_exists feroxbuster; then
     echo "Installing feroxbuster..."
-    sudo apt install feroxbuster -y
+    sudo apt install feroxbuster -y;
 fi
 
 # Check and install Metasploit
 if ! command_exists msfconsole; then
     echo "Installing Metasploit..."
-    sudo apt install metasploit-framework -y
+    sudo apt install metasploit-framework -y;
 fi
 
 # Check and install dig (part of dnsutils)
 if ! command_exists dig; then
     echo "Installing dnsutils (includes dig)..."
-    sudo apt install dnsutils -y
+    sudo apt install dnsutils -y;
 fi
 
 # Check and install snmpwalk (part of snmp)
 if ! command_exists snmpwalk; then
     echo "Installing snmp (includes snmpwalk)..."
-    sudo apt install snmp -y
+    sudo apt install snmp -y;
 fi
 
 # Clone CTFEnum repository and set up ctfenum command
@@ -59,5 +59,5 @@ fi
 if [ ! -f "/usr/sbin/ctfenum" ]; then
     echo "Setting up ctfenum command..."
     echo 'sudo python3 /opt/CTFEnum/CTFenum/CTFenum.py "$1"' | sudo tee /usr/sbin/ctfenum >/dev/null
-    sudo chmod +x /usr/sbin/ctfenum
+    sudo chmod +x /usr/sbin/ctfenum;
 fi
