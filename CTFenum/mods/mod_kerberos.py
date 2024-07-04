1065a561eabaa910927980937c6bb05f1e3403d2
Jose Mario Llado Marti <think@local>
2024-07-04 11:09:01 +0200

    commit

--- CTFenum/mods/mod_kerberos.py
+++ CTFenum/mods/mod_kerberos.py
@@ -1,25 +1,26 @@
 from mods.mod_utils import *
 import subprocess
+import os
 
 def handle_kerberos(ip, dns):
-    filename = '/usr/share/seclists/Usernames/xato-net-10-million-usernames.txt'
-    cmd = f'nmap -p 88 --script=krb5-enum-users --script-args krb5-enum-users.realm="{dns}",userdb={filename} {ip}'
+    filename = '/usr/share/seclists/Usernames/Names/names.txt'
+    cmd = f'nmap -Pn -p 88 --script=krb5-enum-users --script-args krb5-enum-users.realm="{dns}",userdb="{filename}" {ip}'
     output = None
-
     try:
         output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
 
         if output:
-            if 'Discovered' in output:
-                result = re.findall('@n@(Discovered .+@n@@n@)', output.replace('\n', '@n@'))
-
-                if result:
-                    output = result[0].replace('@n@', '\n').replace('\n\n', '')
-
-                    print_banner('88')
-                    print('[!] KERBEROS')
-                    print(cmd)
-                    printc('[+] Some usernames where found.', GREEN)
-                    printc(output, BLUE)
-    except:
+            if ('Discovered' in output):
+                print_banner('88')
+                print('[!] KERBEROS')
+                print(cmd)
+                printc('[+] Some usernames where found.', GREEN)
+                for line in output.splitlines():
+                    if dns in line:
+                        user = line.split('@')[0].split(' ')[-1]
+                        printc(user, BLUE)
+    except Exception as e:
+        if not os.path.exists(filename):
+            print(f'[-] {filename} does not exist.\nPlease install seclists.')
+        printc(e, RED)
         return
