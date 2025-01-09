36f7f192adae27d73a3db0fa4c8bff4668cec2ca
josewdf <60181010+josemlwdf@users.noreply.github.com>
2025-01-09 12:10:56 +0100

    Update mod_smtp.py
    changed username bruteforce tool from nmap script to msfconsole module

--- CTFenum/mods/mod_smtp.py
+++ CTFenum/mods/mod_smtp.py
@@ -2,19 +2,16 @@ import subprocess
 from mods.mod_utils import *
 
 def handle_smtp(ip):
-    cmd = f'nmap -n -T5 -Pn --script="*smtp* and not brute" -p 25 {ip}'
+    cmd = f'msfconsole -q -x "use auxiliary/scanner/smtp/smtp_enum;set RHOSTS {ip};set UNIXONLY false;set USER_FILE /usr/share/seclists/Usernames/xato-net-10-million-usernames.txt;run;exit;"'
     output = None
     
     try:
         output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
         if output:
-            result = re.findall(r'@n@(PORT .+@n@@n@)', output.replace('\n', '@n@'))[0]    
-            if result:
-                print_banner('25')
-                print('[!] SMTP')
-                print(f'[!] {cmd}')
-                print(result.replace('@n@', '\n'))
-
-                log(output, cmd, ip, 'nmap')
+            print_banner('25')
+            print('[!] SMTP')
+            print(f'[!] {cmd}')
+            print(output)
+            log(output, cmd, ip, 'msfconsole')
     except:
         return
